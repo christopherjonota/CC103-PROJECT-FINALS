bb62c0df47c89cfaca12655a6d58762e293eb8c7
Christopher Jonota <cristopherjonota@gmail.com>
2024-04-01 10:39:32 +0800

    Merge branch 'Main-Version' of https://github.com/christopherjonota/CC103-PROJECT-FINALS.git into PATCH1

--- Project_1stYear_2ndSem/src/application/Main.java
+++ Project_1stYear_2ndSem/src/application/Main.java
@@ -18,14 +18,9 @@ import javafx.stage.Stage;
 
 
 public class Main extends Application{
-	public static void main(String[] args)  {
-
-
-		
+	public static void main(String[] args)  {		
 		launch(args);
 	}
-	
-	
 	//Loading Scenes Using JAVAFX
 	@Override
 	public void start(Stage primaryStage) throws Exception {
