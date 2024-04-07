dc491e014d4567a73645e6a85e11a1f51369642d
Angelica <Angelica@LAPTOP-8KJA0K3K>
2024-04-08 01:01:56 +0800

    dfdfd

--- Project_1stYear_2ndSem/src/Scenes/LoginPageController.java
+++ Project_1stYear_2ndSem/src/Scenes/LoginPageController.java
@@ -51,6 +51,11 @@ public class LoginPageController {
 		scene = new Scene(root);
 		stage.setScene(scene);
 		stage.show();
+		
+		System.out.println("Halo");
+		System.out.println("Halo");
+		System.out.println("Halo");
+		
 	}
 	//////////////////////////////////////////////////////////////////////////////////////////////
 	
@@ -70,89 +75,6 @@ public class LoginPageController {
 	//////////////////////////////////////////////////////////////////////////////////////////////
 	// Method for the Login Button in the input area
 	@FXML
-	public void loginClicked(ActionEvent event) throws IOException {
-
-		// trim() - is used to remove whitespaces on the input of the user
-		String usernameInput = login_username.getText().trim();
-		String passwordInput = login_password.getText().trim();
-
-		// If the user do not input something or inputs a white space and click the
-		// login button, this will return.
-		if (login_username.getText().isBlank() || login_password.getText().isBlank()) {
-			loginMessageLabel.setText("Please fill out the fields below.");
-			
-			// This will enable the visibility of the background for the message label
-			loginMessageBackground.setVisible(true);	
-			exitMessageLabel.setVisible(true);
-			
-			// FOR CSS Purposes only
-			if(login_username.getText().isBlank() && login_password.getText().isBlank()) {
-				login_username.getStyleClass().add("borderBox");	// This will add a class inside the css to the fxid so it can have a design
-				login_password.getStyleClass().add("borderBox");
-			}
-			else if (login_username.getText().isBlank()) {
-				login_username.getStyleClass().add("borderBox");
-				login_password.getStyleClass().remove("borderBox");	// This will remove the class added inside the the fxid 
-			}
-			else {
-				login_username.getStyleClass().remove("borderBox");
-				login_password.getStyleClass().add("borderBox");			
-			}
-		}
-		else {
-			try {
-				login_username.getStyleClass().remove("borderBox");
-				login_password.getStyleClass().remove("borderBox");
-				
-				
-				Class.forName("com.mysql.cj.jdbc.Driver");	// This is used for loading mysql Driver				
-				// REMINDER: THIS MUST CHANGE ACCORDING TO YOUR DEVICE ESPECIALLY THE PORT ADDRESS!!
-				// those inside the getConnection parameter is => (Database:Driver://ServerAddress:PortAddress/DatabaseName,Username,Password)
-				Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/payrollsystemdb", "root", "");
-
-				
-				// This will check if there are similar data inside the database according to the input of the user 
-				// This will retrieve the username and password based on the input of the user		
-				// The 'BINARY' function will convert the string to a value which it is used as case-sensitivity checking.
-				String statement = "SELECT count(1) FROM credentials WHERE BINARY username= '" + usernameInput +"' AND BINARY password='"+ passwordInput + "';";
-
-				
-				// This will create and execute the statement
-				Statement stat = con.createStatement();
-				ResultSet queryResult = stat.executeQuery(statement);
-
-				
-				while (queryResult.next()) {
-					if(queryResult.getInt(1) == 1) {
-						// This will switch the page to homepage if validated 
-						root = FXMLLoader.load(getClass().getResource("HomePage.fxml")); 
-						stage = (Stage)((Node)event.getSource()).getScene().getWindow(); 
-						scene = new Scene(root); 
-						stage.setScene(scene); 
-						stage.show();
-					}
-					else {
-						login_username.setText("");
-						login_password.setText("");
-						loginMessageLabel.setText("Incorrect username or password.");
-						loginMessageBackground.setVisible(true);
-						exitMessageLabel.setVisible(true);
-						login_username.getStyleClass().add("borderBox");
-						login_password.getStyleClass().add("borderBox");
-					}		
-				} 
-				
-
-				// This will catch the errors and prints it in the console to be diagnosed
-			} catch (ClassNotFoundException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			} catch (SQLException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-		}
-	}
 	
 	
 	public void exitMessage(ActionEvent event) throws IOException{
